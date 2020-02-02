50ab7bd186609d61521bb551cf87ea99dbede1dc
Mark Cornwell <mark.cornwell@me.com>
2020-02-02 12:32:44 -0500

    adding files from npm build

---
+++ build/precache-manifest.d27d0223c1bd4d2f00303522bca255a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04a34ddad55c21a90e6034675111734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3926a02f4fa41e2e2a05",
+    "url": "/static/css/main.f6a33eae.chunk.css"
+  },
+  {
+    "revision": "08a647bc58d76d87a823",
+    "url": "/static/js/2.a6e3308f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a6e3308f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3926a02f4fa41e2e2a05",
+    "url": "/static/js/main.70aa1f28.chunk.js"
+  },
+  {
+    "revision": "0428dab0a1b266459f0e",
+    "url": "/static/js/runtime-main.56941b36.js"
+  }
+]);
